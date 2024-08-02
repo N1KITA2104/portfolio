1d3ae311ec916f35da9014a441ced81dc99410b9
nikita <herobrineset21@gmail.com>
2024-08-02 22:02:44 +0300

    feat: создать компоненты Header и Footer и установить Bootstrap 5
    - Созданы компоненты Navigation и Footer
    - Добавлен Bootstrap 5 через npm
    - Обновлены файлы angular.json для подключения Bootstrap

--- calculator-converter/src/app/app.module.ts
+++ calculator-converter/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
