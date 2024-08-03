b1906febbc9c3980a1d8cc0fcc3ec6e17ca43d29
nikita <herobrineset21@gmail.com>
2024-08-03 15:58:26 +0300

    Добавлен компонент навигации с иконкой и изменены стили:
    - Добавлен компонент `Navigation` с ссылкой на калькулятор и иконкой.
    - Обновлены стили `styles.scss`:
      - Подключен шрифт Roboto.
      - Установлен фон для `html` и `body` с градиентами.
      - Обновлены стили для `.bg-dark`.

--- calculator-converter/src/app/layout/navigation/navigation.component.ts
+++ calculator-converter/src/app/layout/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
-
 }
