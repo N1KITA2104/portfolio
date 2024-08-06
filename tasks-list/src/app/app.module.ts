eec060f2a621543ffc035cea93c4e7bc0588acf8
nikita <herobrineset21@gmail.com>
2024-08-06 04:11:45 +0300

    feat(added components) added layout components (NavComponent, FooterComponent);

--- tasks-list/src/app/app.module.ts
+++ tasks-list/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { TaskListModule } from './task-list/task-list.module';
+import { NavComponent } from './layout/nav/nav.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
