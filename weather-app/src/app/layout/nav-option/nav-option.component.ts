92a4954d235fd1d31a0d37bd8004c1b4a86145ac
nikita <herobrineset21@gmail.com>
2024-08-10 07:59:47 +0300

    - небольшие изменения в NavOption

--- weather-app/src/app/layout/nav-option/nav-option.component.ts
+++ weather-app/src/app/layout/nav-option/nav-option.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-option',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav-option.component.html',
   styleUrl: './nav-option.component.scss'
 })
