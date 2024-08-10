2d37575220d3cafd990a8ba9e2a2e384e6b632a7
nikita <herobrineset21@gmail.com>
2024-08-10 08:14:42 +0300

    - доработан компонент почасового отображения погоды

--- weather-app/src/app/components/hourly/hourly.component.ts
+++ weather-app/src/app/components/hourly/hourly.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { BaseWeatherComponent } from '../base-weather/base-weather.component';
+import { WeatherForecastService } from '../../services/weather-forecast.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hourly',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hourly.component.html',
   styleUrl: './hourly.component.scss'
 })
-export class HourlyComponent {
-
+export class HourlyComponent extends BaseWeatherComponent {
+  constructor(weatherService: WeatherForecastService) {
+    super(weatherService);
+  }
 }
